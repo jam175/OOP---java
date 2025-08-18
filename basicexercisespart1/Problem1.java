28e7bf556ae3e30e37577bbc39ec2181956d5532
jam175 <jamcanoy566@gmail.com>
2025-08-18 14:43:33 +0800

    Add files via upload

---
+++ basicexercisespart1/Problem1.java
@@ -0,0 +1,20 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicexercisespart1;
+
+/**
+ *
+ * @author User
+ */
+public class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("HELLO"); 
+        System.out.println("Jamella Canoy");
+    }
+ 
+        
+    }
+
---
+++ basicexercisespart1/Problem8.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicexercisespart1;
+
+/**
+ *
+ * @author User
+ */
+public class Problem8 {
+    public static void main(String[] args) {
+        
+    }
+       public class CircleCalculator {
+    public static void main(String[] args) {
+       
+        double radius = 7.5;
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+       
+    }
+}
+
+    }
+ 
+
